251e3eaee0ab570cf5c84f5abb04237963c143de
Chloe <chloe.ronteix@gmail.com>
2020-01-22 13:51:55 +0100

    commit read and prepare sirene

--- Projet_POEC/Projet/read_prepare_csv_SIRENE.py
+++ Projet_POEC/Projet/read_prepare_csv_SIRENE.py
@@ -8,7 +8,8 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 500)
 import numpy as np
 
-df=pd.read_csv('StockEtablissement_utf8.csv')
+df=pd.read_csv('StockEtablissement_utf8.csv', nrows=150)
 print(df.shape)
 
-
+#print(df.head(10))
+print(df.columns)
