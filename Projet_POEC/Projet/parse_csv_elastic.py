c88a6192f8c2a1bf75631aa562f1dcf47b2f93f6
Chloe <chloe.ronteix@gmail.com>
2020-01-23 14:32:19 +0100

    commit parse_csv je ne comprend pas ce que je dois mettre dans la variable my_dict

--- Projet_POEC/Projet/parse_csv_elastic.py
+++ Projet_POEC/Projet/parse_csv_elastic.py
@@ -4,38 +4,19 @@ Created on 23 janv. 2020
 @author: Administrateur
 '''
 
-from Projet import connect_elastic
-from Projet import put_csv_elastic
 from connect_elastic import es
 import csv
 
-# this returns up to 500 rows, adjust to your needs
-res = es.search(index="YourIndexName", body={"query": {"match": {"title": "elasticsearch"}}},500)
+# recherche elastic visee
+res = es.search(index="test", body={"query": { "bool": { "must_not": { "exists": { "field": "coordonnees" }}}}}, size=5)
 sample = res['hits']['hits']
 
-#Deuxieme exemple
-
-# Replace the following Query with your own Elastic Search Query
-res = es.search(index="search", body=
-                {
-                    "_source": ["DTDT", "TRDT", "SPLE", "RPLE"],
-                    "query": {
-                        "bool": {
-                            "should": [
-                                {"wildcard": {"CN": "TEST1"}}
-
-                            ]
-                        }
-                    }
-}, size=10)
-
-
-with open('mycsvfile.csv', 'w') as f:  # Just use 'w' mode in 3.x
-    header_present  = False
-    for doc in res['hits']['hits']:
+with open('mycsvfile.csv', 'w') as csvfile:  # Just use 'w' mode in 3.x
+    header_present = False
+    for doc in sample: 
         my_dict = doc['_source'] 
         if not header_present:
-            w = csv.DictWriter(f, my_dict.keys())
+            w = csv.DictWriter(csvfile, my_dict.keys())
             w.writeheader()
             header_present = True
 
