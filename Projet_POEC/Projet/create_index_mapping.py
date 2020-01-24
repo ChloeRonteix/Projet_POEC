d8e703856978e65484d903333e7ff4fa89aa0fdd
Chloe <chloe.ronteix@gmail.com>
2020-01-24 10:39:11 +0100

    create nouveau mapping

---
+++ Projet_POEC/Projet/create_index_mapping.py
@@ -0,0 +1,33 @@
+'''
+Created on 24 janv. 2020
+
+@author: Administrateur
+'''
+from elasticsearch import Elasticsearch
+from connect_elastic import es
+
+if es.indices.exists(index='test'):
+    es.indices.delete(index='test')
+
+
+request_body = {
+        "settings" : {
+            "number_of_shards": 3,
+            "number_of_replicas": 1
+        },
+        "settings": {"max_result_window": "100000"},
+        'mappings': {
+                'properties': {
+                    'siret':  {'type': 'long'},
+                    'numeroVoieEtablissement':  {'type': 'text'},
+                    'typeVoieEtablissement':  {'type': 'text'},
+                    'libelleVoieEtablissement':  {'type': 'text'},
+                    'codePostalEtablissement':  {'type': 'text'},
+                    'libelleCommuneEtablissement':  {'type': 'text'},
+                    'location': {'type': 'geo_point'}
+                    }}
+}
+
+es.indices.create(index='test', body= request_body)
+
+
