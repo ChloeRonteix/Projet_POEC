eecef531fbd2529ca20ad6b1adf4225c1527f889
Chloe <chloe.ronteix@gmail.com>
2020-01-24 11:23:52 +0100

    commit go_to_api sans la spécification des colonnes d'entrées

--- Projet_POEC/Projet/go_to_API.py
+++ Projet_POEC/Projet/go_to_API.py
@@ -4,22 +4,31 @@ Created on 23 janv. 2020
 @author: Administrateur
 '''
 import requests
+import pandas as pd
 
 files = {
     'data': (open('mycsvfile.csv', 'rb').read())
     }
-
-data = {'columns': ('numeroVoieEtablissement',
+'''
+'columns': ('numeroVoieEtablissement',
                      'typeVoieEtablissement',
                       'libelleVoieEtablissement',
-                       'libelleCommuneEtablissement'),
-        'result_columns': ('latitude','longitude'),
-        'postcode': 'codePostalEtablissement'
+                       'libelleCommuneEtablissement'),'''
+
+data = {
+        'postcode': 'codePostalEtablissement',
+        'result_columns': ('latitude','longitude')
+        
 }
 
 res = requests.post('https://api-adresse.data.gouv.fr/search/csv/', files=files, data=data)
 print(res.text)
 
 
+'''
+res_api = res.text
+print(res_api)
+df = pd.DataFrame(res_api, delimiter=',')
+print(df)'''
 
 
